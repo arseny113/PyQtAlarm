8bbd49ece8bbf8a289916e147ef4da1c67a14fc6
Arseny Gubarkov <arseny@gubarkov.pro>
2021-10-25 23:40:33 +0300

    initial commit Скелет программы с наброском GUI главной формы

---
+++ Alarm.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import win32com.client as win32
+
+
+if __name__ == "__main__" :
+    if len(sys.argv) == 1:
+        pass
+        # открывем окно редактирования
+    else:
+        pass
+        # открываем окно с будильноиком
